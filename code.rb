f65c3e26757d64b3cd330510e466351dda5f29c9
Dara Hoy <darahoy@gmail.com>
2018-10-11 16:19:01 -0400

    Add methods

--- code.rb
+++ code.rb
@@ -1,1 +1,14 @@
-# YOUR CODE HERE
+def plus_two(number)
+  number + 2
+end
+
+def subtract_or_multiply_by_two(number)
+  number < 0 ? number - 2 : number * 2
+end
+
+def repeat_last_word(sentence)
+  new_sentence = "#{sentence} #{sentence.split.last}"
+end
+
+def return_value_of_type(type)
+end
