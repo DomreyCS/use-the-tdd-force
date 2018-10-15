24c8cf3f170bd45c226f8f2e9131d3fab40af5c6
Dara Hoy <darahoy@gmail.com>
2018-10-15 12:52:09 -0400

    Complete write all methods to pass test suite

--- code.rb
+++ code.rb
@@ -11,10 +11,15 @@ def repeat_last_word(sentence)
 end
 
 def return_value_of_type(object)
-  if object.class == String
+  if object == :string
     return object
-  elsif object.class == Fixnum
-    return true
+  elsif object == :fixnum
+    return object.to_s.to_i
+  else
+    "input does not match any Ruby class"
   end
-  
+end
+
+def add_five_and_ten(array)
+  array = [5, 10].each {|number| array << number}
 end
